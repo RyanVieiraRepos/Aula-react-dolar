f7fe4776936f289fd0115b1137acbc50e904967c
RyanVieiraRepos <ryanvieiramail@gmail.com>
2025-08-05 20:37:49 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Titulo from './components/Titulo'
-
+import Footer from './components/footer'
 
 function App() {
   return (
@@ -9,6 +9,8 @@ function App() {
     <div className='container'>
      <Titulo/>
     </div>
+      
+        <Footer/>
       
       
     </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
 
+function Footer(){
+    return(
+    <>
+    <footer className='text-center bg-dark text-white p-3'>
+        <p>
+            Desenvolvido por &nbsp;
+            <a href='' target="blank" className='text-danger'>Ryan Vieira</a>
+         &nbsp; no &nbsp;
+            <a href='' target="blank" className='text-danger'>Senac americana</a>
+        </p>
 
+    </footer>
+    
+    </>
+    
+)
+}
 
 
 export default Footer
